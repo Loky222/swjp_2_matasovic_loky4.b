7618d319c14ee3757deb6831bd880faf0fec0fca
Loky <lovro.bartoli@gmail.com>
2026-03-09 15:59:07 +0100

    aaaa

---
+++ boja-rijec/index.js
@@ -0,0 +1,8 @@
+import randomWords from "starwars-names";
+import randomColor from "randomcolor";
+
+
+const word = randomWords.random();
+const color = randomColor();
+console.log(word);
+console.log(color);
---
+++ one-line-joke/index.js
@@ -0,0 +1,8 @@
+
+
+
+import oneLinerJoke from 'one-liner-joke';
+
+const joke=oneLinerJoke.getRandomJoke();
+
+console.log(joke.body);
